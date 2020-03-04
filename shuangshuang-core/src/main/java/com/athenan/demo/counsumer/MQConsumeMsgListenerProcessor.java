16068cfcd6b9dd91993e3f7f46a397916f138873
shuanshuanshuang <61463803+shuanshuanshuang@users.noreply.github.com>
2020-03-04 17:41:09 +0800

    rocketMq的消费者

--- shuangshuang-core/src/main/java/com/athenan/demo/counsumer/MQConsumeMsgListenerProcessor.java
+++ shuangshuang-core/src/main/java/com/athenan/demo/counsumer/MQConsumeMsgListenerProcessor.java
@@ -1,11 +1,16 @@
 package com.athenan.demo.counsumer;
 
+import com.alibaba.fastjson.JSON;
+import com.atnanjing.demo.dao.Student;
+import com.atnanjing.demo.dao.UserDO;
+import com.atnanjing.demo.mapper.StudentMapper;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.common.message.MessageExt;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
@@ -25,13 +30,21 @@ public class MQConsumeMsgListenerProcessor implements MessageListenerConcurrentl
         for (MessageExt messageExt:msgs ) {
             String msg=new String(messageExt.getBody());
             logger.info("接收到的消息是："+msg);
-            if(messageExt.getTopic().equals("你的topic")){
-                if(messageExt.getTags().equals("你的tag")){
+            if(messageExt.getTopic().equals("DemoTopic")){
+                if(messageExt.getTags().equals("DemoTag")){
                     int reconsumeTimes=messageExt.getReconsumeTimes();
                     if(reconsumeTimes == 3){
                         return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
                     }
                     //以下为业务处理代码
+                    UserDO userDO=JSON.parseObject(msg,UserDO.class);
+                    Student student=new Student();
+                    student.setId(userDO.getId());
+                    student.setAge(userDO.getAge());
+                    student.setName(userDO.getName());
+                    student.setSex(userDO.getSex().toString());
+                    logger.info("接收到的消息为Student"+student);
+
 
 
                 }
---
+++ shuangshuang-core/src/test/java/com/athenan/demo/ShuangshuangcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athenan.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShuangshuangcoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
